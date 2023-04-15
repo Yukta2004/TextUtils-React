a7844d201a8d9ba601e67c10407bc1709560b9cc
Yukta2004 <yuktamore29@gmail.com>
2023-04-15 20:21:16 +0530

    Updates

---
+++ precache-manifest.21f0d8ae268744f14429c0eb4529d678.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c977646604b4fcccd0ac",
+    "url": "/Textutils-React/static/css/main.2090ecc9.chunk.css"
+  },
+  {
+    "revision": "c977646604b4fcccd0ac",
+    "url": "/Textutils-React/static/js/main.c9776466.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/Textutils-React/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/Textutils-React/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "daf74f3886ef26676126",
+    "url": "/Textutils-React/static/js/runtime~main.daf74f38.js"
+  },
+  {
+    "revision": "52ee46db5ec514a753632cf594bcd4f5",
+    "url": "/Textutils-React/index.html"
+  }
+];
